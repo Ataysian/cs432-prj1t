e440c19aa0c54719060630eda6f9d5aa1b299bfc
Alex C. Taylor <20act3@lohani.cs.williams.edu>
2018-10-14 23:59:13 -0400

    updates

--- thread.cc
+++ thread.cc
@@ -20,12 +20,15 @@ typedef struct {
 } thread_t;
 
 typedef struct {
+  unsigned int label;
   thread_t *holder;
-  queue<thread_t> waiters;
-  map< unsigned int, queue<thread_t> > signalWaiters;
+  queue<thread_t*> waiters;
+  map< unsigned int, queue<thread_t> > signalWaiters; //maps CVs to waiters
 } lock_t;
 
 queue<thread_t*> ready;
+thread_t* current_thread;
+map<unsigned int, lock_t*> locks;
 //thread_t *tobe_deleted;
 
 static int create_thread(thread_startfunc_t func, void *arg){
@@ -44,8 +47,20 @@ static int create_thread(thread_startfunc_t func, void *arg){
   ready.push(thread);  
 }
 
+static int run_next_ready(){
+  if(ready.empty()){
+    cout << "thread library exiting" << endl;
+    exit;
+  } else {
+    thread_t *temp = ready.front();
+    ready.pop();
+    swapcontext(&current_thread->uc, &temp->uc);
+  }
+}
+
 static int run_func(thread_t *t){
   interrupt_disable();
+  current_thread = t;
   /*if(*tobe_deleted != null){
     delete tobe_deleted;
     }*/
@@ -59,24 +74,16 @@ static int run_func(thread_t *t){
     tobe_delted = t;
     }*/
   delete t;
-  if(ready.empty()){
-    cout << "thread library exiting" << endl;
-    exit;
-  }else if{
-    thread_t *temp = ready.front;
-    ready.pop;
-    setcontext(&temp->uc);
-  }
+  run_next_ready();
   interrupt_enable;
 }
-
 
 
 int thread_libinit(thread_startfunc_t func, void *arg){
   interrupt_disable();
   create_thread(func, arg);
   interrupt_enable();
-  run_func(ready.front);
+  run_func(ready.front());
 }
 
 int thread_create(thread_startfunc_t func, void *arg){
@@ -88,33 +95,53 @@ int thread_create(thread_startfunc_t func, void *arg){
 int thread_yield(void){
   interrupt_disable();
   if(ready.empty()){
-    return;
+    return 0;
   }
-  
-  thread_t *thread = new thread_t;
-  getcontext(&thread->uc);
-
-  thread->stack = new char [STACK_SIZE];
-  thread->uc.uc_stack.ss_sp = thread->stack;
-  thread->uc.uc_stack.ss_size = STACK_SIZE;
-  thread->uc.uc_stack.ss_flags = 0;
-  thread->uc.uc_link = NULL;
-  
-  makecontext(&thread->uc, (void (*) ()) func, 1, arg);
-
-  ready.push(thread);
+ 
+  ready.push(current_thread);
   thread_t *temp = ready.front();
   ready.pop();
-  swapcontext(thread,temp);
+  swapcontext(&current_thread->uc,&temp->uc);
   interrupt_enable();
 }
 
 int thread_lock(unsigned int lock){
-
+  interrupt_disable();
+  if(locks.find(lock) == locks.end()){ //if lock doesn't exist create it
+    lock_t *newLock = new lock_t;
+    newLock->label = lock;
+    newLock->holder = current_thread;
+    //newLock->waiters = new queue<thread_t>();
+    //newLock->signalWaiters = new map< unsigned int, queue<thread_t> >();
+    locks.insert(pair<unsigned int, lock_t*>(lock, newLock));
+    current_thread->locks.insert(lock);
+  }
+  else if(current_thread->locks.find(lock) == current_thread->locks.end()){ //current thread doesn't have lock
+    locks.find(lock)->second->waiters.push(current_thread); //add current thread to locks waiters queue
+    run_next_ready();
+    locks.find(lock)->second->holder = current_thread;
+    current_thread->locks.insert(lock);
+  }
+  interrupt_enable();
 }
 
 int thread_unlock(unsigned int lock){
-
+  if(current_thread->locks.find(lock) == current_thread->locks.find(lock).end()){ //if current thread doesn't have lock
+    cout << "FAK U" << endl;
+  }
+  else if(locks.find(lock) != locks.end()) { //if lock exists
+    if(locks.find(lock)->second->waiters.empty() && locks.find(lock)->second->signalWaiters.empty()){ // nobody waiting
+      delete(locks.find(lock)->second);
+      locks.erase(lock);
+    }	
+    else { // else: waiter list is not empty
+      ready.push(locks.find(lock)->second->waiters.first());
+      locks.find(lock)->second->waiters.pop();
+    }
+  }
+  else { //if lock doesn't exist
+    
+  }
 }
 
 int thread_wait(unsigned int lock, unsigned int cond){
